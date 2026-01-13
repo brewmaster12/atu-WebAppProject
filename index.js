6aae746f6871ad4fb8a141d647f1f5400009f114
brewmaster12 <seanywg@gmail.com>
2026-01-13 22:42:27 +0000

    Add code to connect to database

--- index.js
+++ index.js
@@ -2,6 +2,25 @@
 const express = require("express");
 const app = express();
 
+//Connect to database:
+const mysql = require('mysql');
+//Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'booksdata'
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ', err);
+  } else {
+    console.log('Connected to database!');
+  }
+});
+
 // Start the server
 app.listen(3000, () => {
     console.log("Server started of port 3000")
