395e492246e26e650e39ef9b185cbcc6db6e2fec
brewmaster12 <seanywg@gmail.com>
2026-01-13 22:19:52 +0000

    Add code to import express and start server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
- 
+// Import express
+const express = require("express");
+const app = express();
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server started of port 3000")
+});
