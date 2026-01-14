cc9a4e54e5a648c20e84fe6bfe9ca7aa98fad182
brewmaster12 <seanywg@gmail.com>
2026-01-14 22:51:01 +0000

    Add post request functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import express
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Connect to database:
 const mysql = require('mysql');
@@ -27,6 +29,31 @@ app.use(express.static("pages"));
 // GET request to retrieve items from the database
 app.get("/shop", function(req, res) {
   const ID = req.query.rec;
+  connection.query("SELECT * FROM books WHERE id = ?", [ID], function (err, rows, fields) {
+    if (err) {
+      console.error('Error retrieving data from database: ', err);
+      res.status(500).send('Error retrieving data from database');
+    } else if (rows.length === 0) {
+      console.error(`No rows found for ID ${ID}`);
+      res.status(404).send(`No product found for ID ${ID}`);
+    } else {
+      console.log('Data retrieved from database!');
+      console.log(rows[0].Title);
+      console.log(rows[0].Author);
+      console.log(rows[0].Publisher);
+      console.log(rows[0].Price);
+      console.log(rows[0].Image);
+      // Inject data into a HTML
+      const bookTitle = rows[0].Title;
+      const bookAuthor = rows[0].Author;
+      res.render("renderProduct.ejs", {product: bookTitle, author: bookAuthor});
+    }
+  });
+});
+
+// POST requests
+app.post("/shop", function(req, res) {
+  const ID = req.body.rec2;
   connection.query("SELECT * FROM books WHERE id = ?", [ID], function (err, rows, fields) {
     if (err) {
       console.error('Error retrieving data from database: ', err);
