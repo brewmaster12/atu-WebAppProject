3290f735beb973f9a32f57b3ef7ccb0c394ba6bc
brewmaster12 <seanywg@gmail.com>
2026-01-14 22:26:10 +0000

    Add app.get functionality

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'booksdata'
+  database: 'g00474316'
 });
 
 // Connect to the database
@@ -23,6 +23,31 @@ connection.connect((err) => {
 
 // Serve static files from the public directory
 app.use(express.static("pages"));
+
+// GET request to retrieve items from the database
+app.get("/shop", function(req, res) {
+  const ID = req.query.rec;
+  connection.query("SELECT * FROM books WHERE id = ?", [ID], function (err, rows, fields) {
+    if (err) {
+      console.error('Error retrieving data from database: ', err);
+      res.status(500).send('Error retrieving data from database');
+    } else if (rows.length === 0) {
+      console.error(`No rows found for ID ${ID}`);
+      res.status(404).send(`No product found for ID ${ID}`);
+    } else {
+      console.log('Data retrieved from database!');
+      console.log(rows[0].Title);
+      console.log(rows[0].Author);
+      console.log(rows[0].Publisher);
+      console.log(rows[0].Price);
+      console.log(rows[0].Image);
+      // Inject data into a HTML
+      const bookTitle = rows[0].Title;
+      const bookAuthor = rows[0].Author;
+      res.render("renderProduct.ejs", {product: bookTitle, author: bookAuthor});
+    }
+  });
+});
 
 // Start the server
 app.listen(3000, () => {
